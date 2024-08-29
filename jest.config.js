d0b94e0291df5aab3099f781f9bd91b1166c5819
RodrigoMindera <rodrigo.diniz@mindera.com>
2024-08-29 15:37:56 +0100

    Remove

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-{
-  "preset": "ts-jest",
-  "testEnvironment": "node",
-  "testMatch": ["**/?(*.)+(spec|test).ts?(x)"],
-  "collectCoverageFrom": ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
-  "coveragePathIgnorePatterns": ["/node_modules/"],
-  "coverageReporters": ["text", "lcov"]
-} 
